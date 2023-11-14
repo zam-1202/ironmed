c04df0d1b7e425e175b6bb9f8cca9fef44975f68
zam-1202 <samuelnorte90@gmail.com>
2023-11-14 19:41:46 +0800

    Add Item Search Bar

--- libs/scripts/master-page/add-item.js
+++ libs/scripts/master-page/add-item.js
@@ -17,6 +17,15 @@ const Category = (() => {
 
     let toUpdate = false;
 
+    var tableConfig = {
+        paging: true,
+        searching: true,
+        ordering: true,
+        search: {
+            regex: true,
+        },
+    };
+
     thisCategory.loadTableData = () => {
         $.ajax({
             type: "GET",
@@ -26,7 +35,50 @@ const Category = (() => {
                 $('.table').DataTable().destroy();
                 $('#tbody_category').html(response);
 
-                $('.table').DataTable();
+                $('.table').DataTable(tableConfig);
+    
+                $(document).ready(function () {
+                var table = $('.table').DataTable();
+                $('.dataTables_filter input')
+                    .off()
+                    .on('input', function () {
+                        $('.table').DataTable().search(this.value, true, false).draw();
+                        var body = $(table.table().body());
+                        body.unhighlight();
+                        body.highlight(table.search());
+                        var searchTerm = this.value.toLowerCase();
+
+
+                        var regex = searchTerm.replace(/[.*+\-?^${}()|[\]\\]/g, '\\$&');
+                        $.fn.dataTable.ext.search.push(function (settings, searchData, index, rowData, counter) {
+                            for (var i = 0; i < searchData.length; i++) {
+                                if (searchData[i].toLowerCase().indexOf(regex) !== -1) {
+                                    return true;
+                                }
+                            }
+                            return false;
+                        });
+
+                        table.draw();
+
+                        // Remove the custom search function
+                        $.fn.dataTable.ext.search.pop();
+
+                        console.log('Search term in console: "' + searchTerm + '"');
+
+                        // Check if the user's search exactly matches a value in the DataTable
+                        var exactMatch = table
+                            .rows({ search: 'applied' })
+                            .data()
+                            .toArray()
+                            .some(row => row.some(value => value.toLowerCase() === searchTerm.toLowerCase()));
+
+                        console.log('Exact match in DataTable:', exactMatch);
+                        console.log('DataTable values:', table.rows({ search: 'applied' }).data().toArray());
+                    });
+
+                });
+
             },
             error: function () {
 
