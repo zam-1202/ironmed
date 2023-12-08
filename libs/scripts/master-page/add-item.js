c278989c7f88aa50c912e305f5c6c2cfcf7468e8
zam-1202 <samuelnorte90@gmail.com>
2023-12-09 02:20:42 +0800

    Location field validation - REGISTER

--- libs/scripts/master-page/add-item.js
+++ libs/scripts/master-page/add-item.js
@@ -333,7 +333,7 @@ const Product = (() => {
     }
 
     thisProduct.save = () => {
-        const regex = /^[a-zA-Z1-9-'.' ]+$/;
+        const regex = /^[a-zA-Z0-9-'.' ]+$/;
         const product_barcode = $('#txt_product_barcode').val();
         const product_name = $('#txt_product_name').val();
         const product_category = $('#slc_product_category').val();
@@ -681,7 +681,7 @@ window.onload = function(){
 
 const validateProductName = () => {
     //Only accepts A-Z (uppercase and lowercase), digits (0-9), single quotation, hyphen, and period
-    const regex = /^[a-zA-Z1-9-'.' ]+$/;
+    const regex = /^[a-zA-Z0-9-'.' ]+$/;
     const productname = $('#txt_product_name').val().trim();
     const pname = document.getElementById('pname');
     const txtProductName = $('#txt_product_name');
@@ -705,7 +705,7 @@ const validateProductName = () => {
 
 const validateLocationName = () => {
     //Only accepts A-Z (uppercase and lowercase), digits (0-9), single quotation, hyphen, and period
-    const regex = /^[a-zA-Z1-9-'.' ]+$/;
+    const regex = /^[a-zA-Z0-9-'.' ]+$/;
     const locationname = $('#txt_location').val().trim();
     const lname = document.getElementById('pname');
     const txtLocationName = $('#txt_location');
