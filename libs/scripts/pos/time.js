3d20a61397e9f324cb278e238bef2373889bc575
zam-1202 <samuelnorte90@gmail.com>
2023-12-24 18:46:10 +0800

    POS time

--- libs/scripts/pos/time.js
+++ libs/scripts/pos/time.js
@@ -1,9 +1,16 @@
 let timeDisplay = document.querySelector(".pos__head__time");
 
 function refreshTime() {
-    let dateString = new Date().toLocaleString("en-US", { timeZone: "Asia/Manila" });
-    let formattedString = dateString.split(',');
-    timeDisplay.innerHTML = formattedString[1];
+    let date = new Date();
+    let options = {
+        timeZone: "Asia/Manila",
+        hour12: true,
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    };
+    let timeString = date.toLocaleTimeString("en-US", options);
+    timeDisplay.innerHTML = timeString;
 }
 
 setInterval(refreshTime, 1000);
