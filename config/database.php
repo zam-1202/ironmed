20efbc8dae146fae67f7448425e8a2ace599a357
zam-1202 <samuelnorte90@gmail.com>
2024-01-02 01:36:25 +0800

    Database config

--- config/database.php
+++ config/database.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = 'localhost';
-$username = 'root';
-$password = '';
-$db_name = 'ironmed';
+$username = 'u597994262_iron_med';
+$password = 'IronMed_2024!';
+$db_name = 'u597994262_ironmed';
 
 $conn = new mysqli($host, $username, $password, $db_name);
 
